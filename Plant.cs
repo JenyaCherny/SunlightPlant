bb9fee12d49c8ecdd26ffef98b3116bbb1fc51a1
Jenya Cherny <jenya.cherny@gmail.com>
2023-03-19 12:11:40 -0400

    ADDED: 1. Plant.cs Model added 2. DB Context added 3. gitignore updated to avoid tracking .dll files

---
+++ Plant.cs
@@ -0,0 +1,10 @@
+﻿public class Plant
+{
+    public int Id { get; set; }
+    public string? PlantName { get; set; }
+    public string? PlantDescription { get; set; }
+    public DateTime DateDelivered { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
+
---
+++ PlantDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+class PlantDb : DbContext
+{
+    public PlantDb(DbContextOptions<PlantDb> options)
+        : base(options) { }
+
+    public DbSet<Plant> Plants => Set<Plant>();
+
+}
